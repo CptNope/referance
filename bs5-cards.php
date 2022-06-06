78191deb2189d87068aa5fc0ebe8e93f4c021723
Jeremy Anderson <100234377+CptNope@users.noreply.github.com>
2022-06-06 14:14:22 -0500

    Updating!

--- bs5-cards.php
+++ bs5-cards.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <!---https://codepen.io/jca2016satx/pen/gOvjgbW?editors=1010--->
+  <!---https://codepen.io/jca2016satx/pen/MWQqWmZ--->
 </head>
 <body>
 
